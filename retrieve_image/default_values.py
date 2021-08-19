9fcf311757e7edff0e5fc17673e81da74ddc6415
Joachim Flottorp <joachimflottorp@gmail.com>
2021-08-19 22:05:24 +0200

    typo

--- retrieve_image/default_values.py
+++ retrieve_image/default_values.py
@@ -1,7 +1,7 @@
 
 # Doubt it has to be stored as '''x''' But pygubu-designer, a tkinter gui designer did it so might aswell follow along.
 class DEFAULT:
-    # Use 127.0.0.1 if databse is stored locally. I believe, gives socket error for me!
+    # Use 127.0.0.1 if database is stored locally. I believe, gives socket error for me!
     SERVER = '''127.0.0.1'''
     PORT = '''3306'''
     DATABASE = '''license'''
